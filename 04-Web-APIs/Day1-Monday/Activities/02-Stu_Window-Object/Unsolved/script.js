83ce604497b236d360103fd2b8e32c2a89bb9e04
richard hopkins <richard.t.hopkins@outlook.com>
2021-03-20 14:45:33 +1100

    Did the first activity of week 4 day 1

--- 04-Web-APIs/Day1-Monday/Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/Day1-Monday/Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,28 @@
 // TODO: What will the following line of code log?
+
+//the following line of code with log (return) the window element to the console.
+//the window element is the highest element in the DOM it is effectivly the browser.
+
 console.log(window);
 
 // TODO: What will the following line of code log?
+
+//the following line of code goes to the window element then navigates the document element and returns it.
+//then the document which is the document that is being shown is logged to the console.
+//in the case the index.html file will be logged to the console.
+
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+
+//the following line of code returns the documentElement of the document. this is the <html> element of our index html.
+//this is then logged to the console.
+
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+
+//the following line of code will log the head element of the windows document element to the console.
+
 console.log(document.head);
 
